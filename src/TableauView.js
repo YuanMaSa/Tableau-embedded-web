d187fb268bec9bfe482cc49184f1d88727e8910e
YuanMaSa <jetlag10030226@gmail.com>
2019-07-02 17:52:10 +0800

    add image

--- src/TableauView.js
+++ src/TableauView.js
@@ -9,7 +9,7 @@ import TableauReport from "tableau-react";
 const TableauView = props => {
     return (
       <TableauReport
-        url="https://10ay.online.tableau.com/t/jonathandashboard/views/0630-new/3?iframeSizedToWindow=true&:embed=y&:showAppBanner=false&:display_count=no&:showVizHome=no&:origin=viz_share_link"
+        url="https://10ay.online.tableau.com/t/jonathandashboard/views/0630-new/5?iframeSizedToWindow=true&:embed=y&:showAppBanner=false&:display_count=no&:showVizHome=no&:origin=viz_share_link"
         // query="?:embed=yes&:comments=no&:toolbar=yes&:refresh=yes"
       />
     );
